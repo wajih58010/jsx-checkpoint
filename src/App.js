7d88e5d32d57b3e4255adbdac512790ace485492
wajih <gabtniwajih123@gmail.com>
2021-02-02 21:26:02 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import JSXImage from './assets/jsx-src.png'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <p className="title red">wajih gabtni</p>
+       <img
+        className="react-logo"
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1280px-React-icon.svg.png"
+        alt="logo react"
+      />
+      <br />
+
+      
+      <img src="jsx.png" alt="" style={{ width: "20%" }} />
+      <br /><br /><br />
+      <img src={JSXImage} alt="" style={{ width: "20%" }} />
+
+      
     </div>
   );
 }
